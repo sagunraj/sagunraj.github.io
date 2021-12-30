fa5f304beba623cd9753ea0f1c8adf5744f104b2
Sagun Raj Lage <sagoonraj@gmail.com>
2021-12-30 16:33:33 +0545

    [add] adds animation in the page

--- js/main.js
+++ js/main.js
@@ -7,14 +7,14 @@
 
         // First hide all content regions, then show the content-region specified in the URL hash 
         // (or if no hash URL is found, default to first menu item)
-        $('.content-region').hide();
+        $('.content-region').fadeOut(100);
 
         // Remove any active classes on the main-menu
         $('.main-menu a').removeClass('active');
         var region = location.hash.toString() || $('.main-menu a:first').attr('href');
 
         // Now show the region specified in the URL hash
-        $(region).show();
+        $(region).fadeIn();
 
         // Highlight the menu link associated with this region by adding the .active CSS class
         $('.main-menu a[href="' + region + '"]').addClass('active');
